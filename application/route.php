88c88f89b1a659fbb4ab36f49d0b9cc280453e0f
ze25800000 <ze258100000@sina.com>
2017-08-08 14:59:08 +0800

    修改路由规则，是匹配精准

--- application/route.php
+++ application/route.php
@@ -7,9 +7,9 @@ Route::get(':version/banner/:id', 'api/:version.Banner/getBanner');
 Route::get(':version/theme', 'api/:version.Theme/getSimpleList');
 Route::get(':version/theme/:id', 'api/:version.Theme/getComplexOne');
 
+Route::get(':version/product/:id', 'api/:version.Product/getOne', [], ['id' => '\d+']);
 Route::get(':version/product/recent', 'api/:version.Product/getRecentProduct');
 Route::get(':version/product/by_category', 'api/:version.Product/getAllInCategory');
-Route::get(':version/product/:id', 'api/:version.Product/getOne');
 
 Route::get(':version/category/all', 'api/:version.Category/getAllCategories');
 
