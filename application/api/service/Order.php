fc63cc7b45aba3b572ef5bbb6d2c1470779c5e04
ze25800000 <ze258100000@sina.com>
2017-08-09 20:42:30 +0800

    order下单接口（3）订单信息与数据库信息比对

--- application/api/service/Order.php
+++ application/api/service/Order.php
@@ -4,16 +4,68 @@ namespace app\api\service;
 
 
 use app\api\model\Product;
+use app\lib\exception\OrderException;
 
 class Order {
     protected $oProducts;
-    protected $product;
+    protected $products;
     protected $uid;
 
     public function place($oProducts, $uid) {
         $this->oProducts = $oProducts;
-        $this->product   = $this->getProductByOrder($oProducts);
+        $this->products  = $this->getProductByOrder($oProducts);
         $this->uid       = $uid;
+        $status          = $this->getOrderStatus();
+    }
+
+    private function getOrderStatus() {
+        $status = [
+            'pass'         => true,
+            'orderPrice'   => 0,
+            'totalCount'   => 0,
+            'pStatusArray' => []
+        ];
+        foreach ($this->oProducts as $oProduct) {
+            $pStatus = $this->getProductStatus($oProduct['product_id'], $oProduct['count'], $this->products);
+            if (!$pStatus['haveStock']) {
+                $status['pass'] = false;
+            }
+            $status['orderPrice'] += $pStatus['totalPrice'];
+            $status['totalCount'] += $pStatus['count'];
+            array_push($status['pStatusArray'], $pStatus);
+        }
+        return $status;
+    }
+
+    private function getProductStatus($oPID, $oCount, $products) {
+        $pIndex  = -1;
+        $pStatus = [
+            'id'         => null,
+            'haveStock'  => false,
+            'count'      => 0,
+            'name'       => '',
+            'totalPrice' => 0
+        ];
+        for ($i = 0; $i < count($products); $i++) {
+            if ($oPID == $products[$i]['id']) {
+                $pIndex = $i;
+            }
+        }
+        if ($pIndex == -1) {
+            throw new OrderException([
+                'msg' => 'id为' . $oPID . '的商品不存在'
+            ]);
+        } else {
+            $product               = $products[$pIndex];
+            $pStatus['id']         = $product['id'];
+            $pStatus['name']       = $product['name'];
+            $pStatus['count']      = $oCount;
+            $pStatus['totalPrice'] = $product['price'] * $oCount;
+            if ($product['stock'] - $oCount >= 0) {
+                $pStatus['haveStock'] = true;
+            }
+        }
+        return $pStatus;
     }
 
     protected function getProductByOrder($oProducts) {
