aa25f58b2c6821220dc657bdb38449ce9d09fea1
ze25800000 <ze258100000@sina.com>
2017-08-12 16:58:08 +0800

    微信支付Pay（4）调用微信支付SDK接口

--- application/api/service/Pay.php
+++ application/api/service/Pay.php
@@ -9,6 +9,10 @@ use app\lib\exception\TokenException;
 use think\Exception;
 use app\api\service\Order as OrderService;
 use app\api\model\Order as OrderModel;
+use think\Loader;
+use think\Log;
+
+Loader::import('WxPay.WxPay', EXTEND_PATH, '.Api.php');
 
 class Pay {
     private $orderID;
@@ -22,13 +26,45 @@ class Pay {
     }
 
     public function pay() {
+        $this->checkOrderValid();
         $orderService = new OrderService();
         $status       = $orderService->checkOrderStock($this->orderID);
         if (!$status['pass']) {
             return $status;
         }
+        return $this->makeWxPreOrder($status['orderPrice']);
     }
 
+    private function makeWxPreOrder($totalPrice) {
+        $openid = Token::getCurrentTokenVar('openid');
+        if (!$openid) {
+            throw new TokenException();
+        }
+        $wxOrderData = new \WxPayUnifiedOrder();
+        $wxOrderData->SetOut_trade_no($this->orderNO);
+        $wxOrderData->SetTrade_type('JSAPI');
+        $wxOrderData->SetTotal_fee($totalPrice * 100);
+        $wxOrderData->SetBody('零食商贩');
+        $wxOrderData->SetOpenid($openid);
+        $wxOrderData->SetNotify_url(config('secure.pay_back_url'));
+        return $this->getPaySignature($wxOrderData);
+    }
+
+    private function getPaySignature($wxOrderData) {
+        $wxOrder = \WxPayApi::unifiedOrder($wxOrderData);
+        if ($wxOrder['return_code'] != 'SUCCESS' || $wxOrder['result_code' != 'SUCCESS']) {
+            Log::record($wxOrder, 'error');
+            Log::record('获取预支付订单失败', 'error');
+        }
+        $this->recordPreOrder($wxOrder);
+        $signature = $this->sign($wxOrder);
+        return $signature;
+    }
+
+    private function recordPreOrder($wxOrder) {
+        OrderModel::where('id', '=', $this->orderID)
+            ->update(['prepay_id' => $wxOrder['prepay_id']]);
+    }
 
     private function checkOrderValid() {
         $order = OrderModel::where('id', '=', $this->orderID)
